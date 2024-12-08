a06e911f8266cc109859ddf739e67e18fb803473
JeseGamerHD <135455727+JeseGamerHD@users.noreply.github.com>
2024-12-08 09:54:15 +0200

    light mode, code cleanup

--- scripts/core.js
+++ scripts/core.js
@@ -1,3 +1,34 @@
+
+/** THIS JS FILE CONTAINS ALL COMMONLY SHARED ELEMENT FUNCTIONALITIES */
+
+let toggleBtn = document.getElementById("mobileSidebarToggle");
+let sidebar = document.getElementById("sideBar");
+let isToggled = false; // Flag for checking if sidebar is active
+function toggleSidebar() {
+    
+    isToggled = !isToggled;
+
+    toggleBtn.classList.toggle("mobileSidebarToggle-open");
+    sidebar.classList.toggle("sideBar-open");
+
+    // Add/Remove a listener for detecting when the user clicks/taps outside of the sidebar to close it
+    if (isToggled) {
+        // Add only when sidebar is active
+        document.getElementById("center").addEventListener("click", toggleSidebar);
+    } 
+    else {
+        // Remove when sidebar is inactive
+        document.getElementById("center").removeEventListener("click", toggleSidebar);
+    }
+}
+
+function closeSidebar() {
+    
+    if(isToggled) {
+        toggleSidebar();
+    }
+}
+
 // The current theme, default to Cosmic.
 let currentTheme = localStorage.getItem("theme") != null ? localStorage.getItem("theme") : "Cosmic";
 
