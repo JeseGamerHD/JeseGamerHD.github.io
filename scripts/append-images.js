8694013cb819e1eeadd15689dae36f7c86296b3d
JeseGamerHD <135455727+JeseGamerHD@users.noreply.github.com>
2024-12-31 18:16:06 +0200

    prism js support, this website page progress, index.html text update

--- scripts/append-images.js
+++ scripts/append-images.js
@@ -22,26 +22,26 @@ function appendImages(imageName) {
 }
 
 function initializeLazyLoading() {
-    
-    const targets = document.querySelectorAll(".screenshot");
 
-    const lazyLoad = target => {
-      const io = new IntersectionObserver((entries, observer) => {
-        entries.forEach(entry => {
-            
-            if (entry.isIntersecting) {
-            const img = entry.target;
-            const src = img.getAttribute('data-src');
-            
-            img.setAttribute('src', src);
-            img.classList.add('fade');
-            observer.disconnect();
-          }
-        });
+  const targets = document.querySelectorAll(".screenshot");
+
+  const lazyLoad = target => {
+    const io = new IntersectionObserver((entries, observer) => {
+      entries.forEach(entry => {
+
+        if (entry.isIntersecting) {
+          const img = entry.target;
+          const src = img.getAttribute('data-src');
+
+          img.setAttribute('src', src);
+          img.classList.add('fade');
+          observer.disconnect();
+        }
       });
-    
-      io.observe(target)
-    };
-    
-    targets.forEach(lazyLoad);
+    });
+
+    io.observe(target)
+  };
+
+  targets.forEach(lazyLoad);
 }
